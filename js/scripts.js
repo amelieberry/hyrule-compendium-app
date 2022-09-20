82e87b4cb75abb509eb671e795a15af2b00e5ec8
Amelie <aac.berry@gmail.com>
2022-09-20 12:42:16 +0200

    added pokemonList array of objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-alert('Hello world!');
-
-let favoriteFood = 'Fatteh';
-document.write("My favorite food is " + favoriteFood);
-
-
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+    {name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
+    {name: 'Venusaur', height: 2, types: ['grass', 'poison'] },
+];
