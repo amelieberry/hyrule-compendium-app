7c23c3e740aa816834b8da675e9ef78860a25f35
Amelie <aac.berry@gmail.com>
2022-09-21 13:09:05 +0200

    added for loop to write array items to doc

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-    {name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
-    {name: 'Venusaur', height: 2, types: ['grass', 'poison'] },
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+    { name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
+    { name: 'Venusaur', height: 2, types: ['grass', 'poison'] },
+    { name: 'Charmander', height: 0.6, types: ['fire'] }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    const container = document.getElementById('compendium-container');
+    const typeString = (pokemonList[i].types.length > 1) ? "types: " : "type: ";
+    document.write('<div class="compendium-item">' + pokemonList[i].name + '<br>' + 'height: ' + pokemonList[i].height + '<br>' + typeString + pokemonList[i].types.join(' and ') + '<br><br>' + '</div>')
+}
