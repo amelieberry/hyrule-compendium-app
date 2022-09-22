e86dde883bd5043e9490517676a06378fcc5df87
Amelie <aac.berry@gmail.com>
2022-09-22 16:53:42 +0200

    added functions to add to and get items from array

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,35 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-    { name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
-    { name: 'Venusaur', height: 2, types: ['grass', 'poison'] },
-    { name: 'Charmander', height: 0.6, types: ['fire'] }
-];
+let pokemonRepository = (function() {
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+        { name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
+        { name: 'Venusaur', height: 2, types: ['grass', 'poison'] },
+        { name: 'Charmander', height: 0.6, types: ['fire'] }
+    ];
 
-// for (let i = 0; i < pokemonList.length; i++) {
-//     const typeString = (pokemonList[i].types.length > 1) ? "types: " : "type: ";
-//     document.write('<div class="compendium-item">' 
-//     + "<h2>" + pokemonList[i].name + "</h2>"
-//     + '<p> height: ' + pokemonList[i].height + "</p>"
-//     + '<p>' + typeString + pokemonList[i].types.join(' and ') + '</p> </div>')
-// }
+    // fetch pokemon array
+    function getAll() {
+        return pokemonList;
+    };
 
-pokemonList.forEach(function(item) {
+    //add pokemon to array
+    function add(item) {
+        if (typeof item !== 'object') {
+            console.log('Should be an object')
+        } else if (!('name' in item) || !('height' in item) || !('types' in item)) {
+            console.log('Invalid keys');
+        }else {
+            pokemonList.push(item);
+        }
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
+
+// go through array of pokemons and write the values on document
+pokemonRepository.getAll().forEach(function(item) {
     const typeString = (item.types.length > 1) ? "types: " : "type: ";
     document.write('<div class="compendium-item">' 
     + "<h2>" + item.name + "</h2>"
